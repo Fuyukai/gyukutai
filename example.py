2720cc362abb8b3d4a891bc79b3c14849302067f
martmists <martmists@gmail.com>
2017-02-11 10:42:02 +0100

    Update README.md and example.py

--- example.py
+++ example.py
@@ -5,11 +5,11 @@ gyukutai.apply()
 from math import sqrt
 
 # apply
-print("Squares of the first 10 integers:", list(range(1, 11)).apply | (lambda x: x ** 2))
+print("Squares of the first 10 integers:", range(1, 11).apply | (lambda x: x ** 2))
 
 # find
-print("First square number:", list(range(2, 100)).find | (lambda x: sqrt(x) % 1 == 0))
+print("First square number:", range(2, 100).find | (lambda x: sqrt(x) % 1 == 0))
 
 # filter
-print("Factors of 234:", list(range(0, 234)).filter | (lambda x: x != 0 and 234 % x == 0))
+print("Factors of 234:", range(0, 234).filter | (lambda x: x != 0 and 234 % x == 0))
 
